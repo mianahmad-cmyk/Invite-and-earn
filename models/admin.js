770954786eef2a18e5a7a17881b634deb721a3ec
Ahmad <miansahib2002@gmail.com>
2025-05-17 10:04:55 +0500

    First commit

--- models/admin.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  balance: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Admin', adminSchema);
